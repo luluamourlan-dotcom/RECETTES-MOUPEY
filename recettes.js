8184b2ecc9e43b4fe738d44fdcf924213b80a34e
luluamourlan-dotcom <luluamourlan@gmail.com>
2026-08-26 00:59:59 +0200

    Update recettes.js

--- recettes.js
+++ recettes.js
@@ -199,6 +199,192 @@ const recettes = [
     //  FIN COUSCOUS
     // ========================================================
 
+
+
+// ========================================================
+    // COUSCOUS Exemple Entrée
+    // ========================================================
+
+    {
+
+        id: "couscous exemple entrée",
+
+        nom: "couscous exemple entrée",
+
+        emoji: "🍲",
+
+        categorie: "entree",
+
+        date: "Juin 2026",
+
+        personnes: "6 personnes",
+
+
+        description:
+            "Couscous xxxxxx.",
+
+
+        image:
+           "images/Plats/couscous.jpg",
+
+
+        recherche:
+            "couscous poulet merguez chorizo légumes carottes navets poireaux courgettes citrouille céleri tomates pois chiches semoule raisins épices harissa",
+
+
+        // ====================================================
+        // VARIANTE
+        // ====================================================
+
+        variante: {
+
+            titre:
+                "Variante andalouse",
+
+            texte:
+                "On remplace les pilons de poulet par 1 kg de pollo de pincho que l’on fait cuire à part dans une poêle."
+
+        },
+
+
+        // ====================================================
+        // INGREDIENTS
+        // ====================================================
+
+        ingredients: [
+
+
+            {
+
+                categorie: "Viandes",
+
+                items: [
+
+                    "12 pilons de poulet",
+
+                    "12 merguez ou saucisses épicées",
+
+                    "1 morceau de chorizo"
+
+                ]
+
+            },
+
+
+            {
+
+                categorie: "Légumes",
+
+                items: [
+
+                    "2 oignons",
+
+                    "3 gousses d’ail",
+
+                    "6 carottes",
+
+                    "6 petits navets",
+
+                    "3 poireaux",
+
+                    "2 courgettes",
+
+                    "1 tranche de citrouille",
+
+                    "2 branches de céleri",
+
+                    "2 belles tomates",
+
+                    "400 g de pois chiches (1 grande boîte)",
+
+                    "5 verres de semoule (grain moyen)",
+
+                    "1 bol de raisins secs"
+
+                ]
+
+            },
+
+
+            {
+
+                categorie: "Épices",
+
+                items: [
+
+                    "1 bâton de cannelle",
+
+                    "1 morceau de gingembre pelé",
+
+                    "1 c. à soupe de cumin en poudre",
+
+                    "1 c. à café de grains de coriandre",
+
+                    "1 c. à café de curcuma",
+
+                    "1 c. à café de noix de muscade moulue",
+
+                    "1 c. à café de safran",
+
+                    "2 clous de girofle",
+
+                    "1/2 c. à café de piment de Cayenne",
+
+                    "Poivre noir, blanc et de Jamaïque",
+
+                    "Harissa"
+
+                ]
+
+            }            
+        ],
+
+
+        // ====================================================
+        // PREPARATION
+        // ====================================================
+
+        preparation: [
+
+            "Ébouillantez les saucisses trois bonnes minutes et réservez.",
+
+            "Pelez et coupez grossièrement les oignons. Lavez puis coupez la branche de céleri en bâtonnets. Râpez les tomates.",
+
+            "Dans la cocotte minute, faites fondre le chorizo puis faites revenir doucement les oignons. Rajoutez les gousses d'ail non pelées et le céleri. Quand le mélange commence à attacher, rajoutez la tomate et les épices, à l’exception de la harissa. Mélangez et laissez mijoter quelques minutes, rajoutez un bon litre d’eau, salez et portez à ébullition.",
+
+            "Pendant ce temps, pelez les carottes, les navets et les courgettes, coupez-les en bâtonnets. Lavez les poireaux et coupez-les en deux. Pelez la citrouille et coupez-la en gros cubes. Égouttez et rincez les pois chiches.",
+
+            "Mettez le poulet dans le bouillon, fermez la cocotte et faites cuire à la vapeur une dizaine de minutes. Rajoutez les carottes, les navets et les poireaux, faites cuire à la vapeur une dizaine de minutes supplémentaires.",
+
+            "Ouvrez la cocotte. Rajoutez les courgettes et laissez mijoter à petit bouillon jusqu’à ce que les courgettes soient cuites sans se défaire. Mettez une louche de bouillon sur les raisins secs pour les faire gonfler.",
+
+            "Retirez délicatement les légumes, déposez-les dans le plat de service. Déposez le poulet dans un autre plat.",
+
+            "Goûtez le bouillon et rectifiez éventuellement en sel et épices. Rajoutez de l’eau si nécessaire et laissez mijoter à feu doux.",
+
+            "Versez la semoule dans un saladier, ajoutez le même volume d’eau bouillante, salez, couvrez et laissez absorber cinq minutes.",
+
+            "Pendant ce temps, faites cuire la citrouille dans le bouillon avec les pois chiches. Faites griller les saucisses.",
+
+            "Rajoutez une bonne cuillère à soupe d’huile d’olive à la semoule et égrenez-la à la fourchette.",
+
+            "Déposez la citrouille, les pois chiches et les raisins dans le plat de légumes.",
+
+            "Préparez une tasse d’harissa avec un peu de bouillon.",
+
+            "Présentez à la table le couscous : les trois plats de graine, légumes et viandes, un bol de bouillon et la harissa.",
+
+            "Bon appétit !"
+
+        ]
+
+
+    }
+
+ // ========================================================
+    //  FIN COUSCOUS
+    // ========================================================
+    
 ];
 
 
